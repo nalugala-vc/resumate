c245e18d2110f2e50f8beec8c8be7d50893a2ff3
nalugala_vc <vanessachebukwa@gmail.com>
2025-05-02 03:24:32 +0300

    Upload resume UI

---
+++ resumate_flutter/lib/features/ai/view/pages/upload_resume.dart
@@ -0,0 +1,161 @@
+import 'dart:io';
+import 'package:dotted_border/dotted_border.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:heroicons/heroicons.dart';
+import 'package:resumate_flutter/core/utils/fonts/sf_pro_display.dart';
+import 'package:resumate_flutter/core/utils/spacers/spacers.dart';
+import 'package:resumate_flutter/core/utils/theme/app_pallette.dart';
+import 'package:resumate_flutter/core/utils/widgets/notifications_icon.dart';
+import 'package:resumate_flutter/core/utils/widgets/rounded_button.dart';
+import 'package:resumate_flutter/features/ai/view/widgets/analyzing_popup.dart';
+
+class UploadResumePage extends StatefulWidget {
+  @override
+  _UploadResumePageState createState() => _UploadResumePageState();
+}
+
+class _UploadResumePageState extends State<UploadResumePage> {
+  File? _selectedFile;
+
+  void _pickResume() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['pdf', 'doc', 'docx'],
+    );
+
+    if (result != null && result.files.single.path != null) {
+      setState(() {
+        _selectedFile = File(result.files.single.path!);
+      });
+    }
+  }
+
+  Widget _buildDashedContainer() {
+    return DottedBorder(
+      borderType: BorderType.RRect,
+      radius: Radius.circular(16),
+      dashPattern: [6, 4],
+      color: AppPallete.primary400,
+      strokeWidth: 1.5,
+      child: InkWell(
+        onTap: _pickResume,
+        child: Container(
+          width: double.infinity,
+          height: 300,
+          padding: EdgeInsets.all(20),
+          child: Center(
+            child:
+                _selectedFile == null
+                    ? Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 150,
+                          height: 150,
+                          decoration: BoxDecoration(
+                            color: AppPallete.primary100,
+                            shape: BoxShape.circle,
+                          ),
+                          child: HeroIcon(
+                            HeroIcons.arrowUpTray,
+                            style: HeroIconStyle.solid,
+                            color: AppPallete.primary400,
+                            size: 34,
+                          ),
+                        ),
+                        spaceH20,
+                        Text(
+                          "Upload Resume",
+                          style: TextStyle(
+                            color: Colors.grey[700],
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    )
+                    : Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          _selectedFile!.path.endsWith('.pdf')
+                              ? Icons.picture_as_pdf
+                              : Icons.description,
+                          size: 60,
+                          color: AppPallete.primary400,
+                        ),
+                        spaceH10,
+                        Text(
+                          _selectedFile!.path.split('/').last,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAnalyzeButton() {
+    if (_selectedFile == null) return SizedBox.shrink();
+
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: RoundedButton(
+        label: 'analyze my resume',
+        onTap: () {
+          showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (context) => const AnalyzingPopup(),
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SfProDisplay(
+                        text: 'Upload Resume',
+                        fontSize: 26,
+                        fontWeight: FontWeight.w700,
+                      ),
+                      SfProDisplay(
+                        text: "Get feedback tailored to you!",
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        textColor: AppPallete.greyColor,
+                        lineheight: 0,
+                      ),
+                    ],
+                  ),
+                  NotificationsIcon(),
+                ],
+              ),
+
+              spaceH30,
+              _buildDashedContainer(),
+              spaceH20,
+              _buildAnalyzeButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ resumate_flutter/lib/features/ai/view/widgets/analyzing_popup.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:resumate_flutter/core/utils/fonts/sf_pro_display.dart';
+import 'package:resumate_flutter/core/utils/spacers/spacers.dart';
+
+class AnalyzingPopup extends StatefulWidget {
+  const AnalyzingPopup({super.key});
+
+  @override
+  State<AnalyzingPopup> createState() => _AnalyzingPopupState();
+}
+
+class _AnalyzingPopupState extends State<AnalyzingPopup>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )..repeat(reverse: false);
+
+    _animation = Tween<double>(
+      begin: 0.0,
+      end: 0.9,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: Colors.white,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      insetPadding: const EdgeInsets.all(24),
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 24),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SfProDisplay(
+                  text: 'Analyzing Your Resume',
+
+                  fontWeight: FontWeight.w700,
+                  fontSize: 18,
+                  textColor: Colors.black,
+                ),
+                spaceH10,
+
+                const Text(
+                  'Just reading through your resume',
+                  style: TextStyle(color: Colors.grey, fontSize: 14),
+                ),
+                spaceH30,
+                SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: AnimatedBuilder(
+                    animation: _animation,
+                    builder: (context, child) {
+                      return Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          SizedBox(
+                            height: 200,
+                            width: 200,
+                            child: CircularProgressIndicator(
+                              value: _animation.value,
+                              strokeWidth: 14, // Thicker circular stroke
+                              valueColor: const AlwaysStoppedAnimation<Color>(
+                                Color(0xFF9CE2E8),
+                              ),
+                              backgroundColor: const Color(0xFFE5F6F8),
+                            ),
+                          ),
+                          const Text(
+                            'ANALYZING',
+                            style: TextStyle(
+                              color: Color(0xFF67C8D3),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              letterSpacing: 1.2,
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 8,
+            right: 8,
+            child: InkWell(
+              onTap: () => Navigator.of(context).pop(),
+              child: const Icon(Icons.close, size: 20, color: Colors.black),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
