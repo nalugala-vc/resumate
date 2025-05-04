4956b6e65dacde3ed62125a1156b37192221af12
nalugala_vc <vanessachebukwa@gmail.com>
2025-05-05 01:01:16 +0300

    Changed the app icon and splash screen

--- resumate_flutter/lib/main.dart
+++ resumate_flutter/lib/main.dart
@@ -1,36 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:resumate_flutter/core/di/app_bindings.dart';
 import 'package:resumate_flutter/core/utils/theme/app_theme.dart';
-import 'package:resumate_flutter/core/utils/widgets/custom_nav_bar.dart';
 import 'package:resumate_flutter/core/utils/widgets/splash_screen.dart';
-import 'package:resumate_flutter/features/ai/view/pages/upload_resume.dart';
-import 'package:resumate_flutter/features/auth/view/otp.dart';
-import 'package:resumate_flutter/features/auth/view/sign_in.dart';
 import 'package:resumate_flutter/features/auth/viewmodel/auth_controller.dart';
-import 'package:resumate_flutter/features/feed/view/pages/homepage.dart';
-import 'package:resumate_flutter/features/feed/view/pages/job_details.dart';
-import 'package:resumate_flutter/features/feed/view/pages/job_matches.dart';
-import 'package:resumate_flutter/features/feed/view/pages/roadmap.dart';
-import 'package:resumate_flutter/features/feed/view/pages/video_player.dart';
-import 'package:resumate_flutter/features/mentorship/view/pages/explore_mentors_page.dart';
-import 'package:resumate_flutter/features/ai/view/pages/AI_chat.dart';
-import 'package:resumate_flutter/features/quiz/view/pages/career_quiz_banner.dart';
-import 'package:resumate_flutter/features/quiz/view/pages/quiz_page.dart';
-import 'package:resumate_flutter/features/quiz/view/pages/test_results.dart';
-
-/* UNFORMATTED PAGES
-
-VIDEO PLAYER PAGE
-QUIZ PAGE*/
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Get.putAsync(() async {
     final controller = SignInController();
-    await controller.loadUserFromStorage(); // Load on app start
+    await controller.loadUserFromStorage();
     return controller;
   });
 
